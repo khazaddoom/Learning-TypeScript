ec25e39dfb92e3ddb75df18bf6a2ba6e1a5dc5a9
GaneshSAcharya <31121986g@gmail.com>
2018-07-18 09:26:07 +0530

    Learning Interfaces 2

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-import { Person } from './person.interface';
-
+"use strict";
+exports.__esModule = true;
 // let example1: string | number = "Ganesh";
-
 // let example2: undefined = undefined;
-
 // example1 = 'asdasd';
-
 // let arr: number[][] = [[1,2,3]];
-
 // const test: [string, number] = ['asdasd', 30,];
-
 // function returnTypes(){
-
 //     return '100';
-
 // }
-
-var ganesh: Person = {firstName : 'Ganesh'};
-
-
-
+var ganesh = { firstName: 'Ganesh' };
 console.log(ganesh.firstName);
