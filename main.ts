8b1def5a0d8b056406a3276da9e3b4c379ca2877
GaneshSAcharya <31121986g@gmail.com>
2018-07-18 09:20:43 +0530

    learning interfaces in TS

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+import { Person } from './person.interface';
+
 let example1: string | number = "Ganesh";
 
 let example2: undefined = undefined;
@@ -5,3 +7,17 @@ let example2: undefined = undefined;
 example1 = 'asdasd';
 
 let arr: number[][] = [[1,2,3]];
+
+const test: [string, number] = ['asdasd', 30,];
+
+function returnTypes(){
+
+    return '100';
+
+}
+
+// var ganesh: Person;
+
+// ganesh.firstName = 'Ganesh';
+
+// console.log(ganesh);
